a05e0e5f835e4cfd677cd2fd779d4e19c79a0b6d
Codekup <codekup@gmail.com>
2017-06-26 22:47:12 +0200

    Added image

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 
 $(document).ready(function () {
 
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    $('.main').css('width', w + 'px');
+    $('.main').css('height', (h - $('.header').height()) + 'px');
+
     $('.header .container div').hover(function () {
         $(this).addClass('header-btn-highlight');
     }, function () {
