a7f2e7e183db890e3acafa3ede980e0342bdcea4
Codekup <codekup@gmail.com>
2017-06-29 19:17:34 +0200

    New design using Bootstrap

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-
 $(document).ready(function () {
-
-    var w = window.innerWidth;
     var h = window.innerHeight;
-
-    $('.main').css('width', w + 'px');
-    $('.main').css('height', (h - $('.header').height()) + 'px');
-
-    $('.header .container div').hover(function () {
-        $(this).addClass('header-btn-highlight');
-    }, function () {
-        $(this).removeClass('header-btn-highlight');
-    });
-
+    $('.main').css('height', h + 'px');
+    $('.main .row').css('height', (h - 70) * 0.75 + 'px');
 });
