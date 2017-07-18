8e688d8c3c9acb2587713d3376337976380f23cc
Codekup <codekup@gmail.com>
2017-07-19 01:12:38 +0200

    Added index footer

--- main.js
+++ main.js
@@ -1,25 +1,30 @@
+var continue_hidden = false;
 var info_text_offset = 0;
 var info_text_offseted = false;
 
 $(document).ready(function () {
     var wh = window.innerHeight;
     var hh = $('#header').height();
-    $('#main-title').css({'height': (wh - hh) + 'px'});
+
+    $('#main-title').css({'height': ((wh - hh) * 0.9) + 'px'});
+    $('#continue').css({'height': ((wh - hh) * 0.1) + 'px'});
+    $('#continue-support').css({'height': ((wh - hh) * 0.1) + 'px'});
 
     $info_text = $('#info-text');
-    $info_text.css({'height': (wh * 0.25) + 'px'});
+    $info_text.css({'height': (wh * 0.2) + 'px'});
     info_text_offset = $info_text.offset().top;
 
-    $('.scroll-div').css({'height': (wh * 0.75) + 'px'});
+    $('.scroll-div').css({'height': (wh * 0.8) + 'px'});
 
     $(window).scroll(function () {
         var distance = info_text_offset - $(document).scrollTop();
+
         if (distance <= 0) {
             if (!info_text_offseted) {
                 $info_text.css({
                     'position': 'fixed'
                 });
-                $('#main-title').after('<div style="height:' + (wh * 0.25) + 'px;"></div>');
+                $('#main-title').after('<div style="height:' + (wh * 0.2) + 'px;"></div>');
                 info_text_offseted = true;
             }
         } else {
@@ -29,6 +34,18 @@ $(document).ready(function () {
                     'position': 'static'
                 });
                 info_text_offseted = false;
+            }
+        }
+
+        if (distance < (hh + (wh - hh) * 0.9)) {
+            if (!continue_hidden) {
+                $('#continue').hide();
+                continue_hidden = true;
+            }
+        } else {
+            if (continue_hidden) {
+                $('#continue').show();
+                continue_hidden = false;
             }
         }
     });
