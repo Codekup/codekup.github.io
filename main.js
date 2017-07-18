b5cf481e91b740d9687577e002f75f72dd26b847
Codekup <codekup@gmail.com>
2017-07-18 02:22:01 +0200

    Added info text

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
+var info_text_offset = 0;
+var info_text_offseted = false;
+
 $(document).ready(function () {
     var wh = window.innerHeight;
     var hh = $('#header').height();
-    $('#main-title').css('height', (wh - hh) + 'px');
+    $('#main-title').css({'height': (wh - hh) + 'px'});
+
+    $info_text = $('#info-text');
+    $info_text.css({'height': (wh * 0.25) + 'px'});
+    info_text_offset = $info_text.offset().top;
+
+    $('.scroll-div').css({'height': (wh * 0.75) + 'px'});
+
+    $(window).scroll(function () {
+        var distance = info_text_offset - $(document).scrollTop();
+        if (distance <= 0) {
+            if (!info_text_offseted) {
+                $info_text.css({
+                    'position': 'fixed'
+                });
+                $('#main-title').after('<div style="height:' + (wh * 0.25) + 'px;"></div>');
+                info_text_offseted = true;
+            }
+        } else {
+            if (info_text_offseted) {
+                $('#main-title').next().remove();
+                $info_text.css({
+                    'position': 'static'
+                });
+                info_text_offseted = false;
+            }
+        }
+    });
 });
